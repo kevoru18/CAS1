29d626e4da6858980dddd81c06e77998586c2682
Kevin Rubí <kevoru@gmail.com>
2020-05-01 19:44:27 -0600

    Se realizo cambios a la actualiza pedido falta corregir error de insert

--- comprassigua/actualiza_pedido.php
+++ comprassigua/actualiza_pedido.php
@@ -9,13 +9,48 @@ $codigo= (isset($_POST['producto'])) ? $_POST['producto'] : "";
 $cantidad= (isset($_POST['canti'])) ? $_POST['canti'] : "";
 $precio=(isset($_POST['precio'])) ? $_POST['precio'] : ""; 
 
-echo $factura; echo $codigo ; echo $cantidad;echo $precio; 
+
 $actualizada="UPDATE temp SET cantidad= :cantidad, costo_total= :sub, estado='Procesado'
  WHERE codigo_pedido=:factura AND codigo_producto=:codigo";
 
 $actualizo=$base->prepare($actualizada);
 $actualizo->execute(array(":cantidad"=>$cantidad,":sub"=>$precio, ":factura"=>$factura, ":codigo"=>$codigo));
+//Agregar a nueva tabla
+$busquedavalores=$base->query("SELECT * FROM temp WHERE codigo_pedido='$factura'")->fetchAll(PDO::FETCH_OBJ);
+foreach ($busquedavalores as $agrego) {
+    $factutras=$agrego->codigo_pedido;
+    $codigotras=$agrego->codigo_producto;
+    //$desctras=$agrego->descripcion_producto;
+    $costotras=$agrego->costoU;
+    $cantidadtras=$agrego->cantidad;
+    $totaltras=$agrego->costo_total;
+    $proveedor=$agrego->codigo_proveedor;
+    $user=$agrego->usuario;
+    $estado="Pendiente";
+    echo $cantidadtras;
+    echo $totaltras;
+    echo $proveedor;
+    
 
+    
+    /*
+  try {
+        //code...
+        
+        $agregoaordenpedido="INSERT INTO orden_pedido(num_factura, cantidad, costo, nombre_usuario, 
+            proveedor, subtotal, estado)
+        VALUES(:myfactu, :mycanti, :mycosto, :myuser, :myproveedor, :mysub, :myestado)";
+        $resultado = $base->prepare($agregoaordenpedido);
+        $resultado->execute(array(":myfactu"=>$factutras, ":mycanti"=>$cantidadtras,
+            ":mycosto"=>$costotras,  ":myuser"=>$user, ":myproveedor"=>$proveedor, 
+            ":mysub"=> $totaltras, ":myestado"=>$estado));
+    } catch (Exception $e) {
+        //throw $th;
+        die("Error: " . $e->getMessage());
+    }
+
+*/
+}
 
 
 ?>
